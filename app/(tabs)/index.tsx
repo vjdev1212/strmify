93c59afe8b14e0cb32d36ebe45d92988c777c232
Jarvis <home80jarvis@gmail.com>
2024-12-10 09:41:31 +0530

    Minor adjustments

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,20 +1,16 @@
 import { SafeAreaView, ScrollView, StyleSheet } from 'react-native';
 import PosterList from '@/components/PosterList';
+import { CatalogUrl } from '@/constants/Stremio';
 
 export default function HomeScreen() {
-  const catalogUrl = {
-    topMovies: 'https://v3-cinemeta.strem.io/catalog/movie/top.json',
-    topSeries: 'https://v3-cinemeta.strem.io/catalog/series/top.json',
-    popularMovies: 'https://cinemeta-catalogs.strem.io/imdbRating/catalog/movie/imdbRating.json',
-    popularSeries: 'https://cinemeta-catalogs.strem.io/imdbRating/catalog/series/imdbRating.json'
-  }
+
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView showsVerticalScrollIndicator={false}>
-        <PosterList apiUrl={catalogUrl.topMovies} title='Top Movies' type='movie' />
-        <PosterList apiUrl={catalogUrl.topSeries} title='Top Series' type='series' />
-        <PosterList apiUrl={catalogUrl.popularMovies} title='Popular Movies' type='movie' />
-        <PosterList apiUrl={catalogUrl.popularSeries} title='Popular Series' type='series' />
+        <PosterList apiUrl={CatalogUrl.topMovies} title='Movies - Top' type='movie' />
+        <PosterList apiUrl={CatalogUrl.topSeries} title='Series - Top' type='series' />
+        <PosterList apiUrl={CatalogUrl.popularMovies} title='Movies - Popular' type='movie' />
+        <PosterList apiUrl={CatalogUrl.popularSeries} title='Series - Popular' type='series' />
       </ScrollView>
     </SafeAreaView>
   );
---
+++ constants/Stremio.ts
@@ -0,0 +1,6 @@
+export const CatalogUrl = {
+    topMovies: 'https://v3-cinemeta.strem.io/catalog/movie/top.json',
+    topSeries: 'https://v3-cinemeta.strem.io/catalog/series/top.json',
+    popularMovies: 'https://cinemeta-catalogs.strem.io/imdbRating/catalog/movie/imdbRating.json',
+    popularSeries: 'https://cinemeta-catalogs.strem.io/imdbRating/catalog/series/imdbRating.json'
+}
