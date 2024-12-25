5a0fbda4729d0aee000df76ad3777b46feda313d
Jarvis <home80jarvis@gmail.com>
2024-12-25 18:07:50 +0530

    delete unused imports

--- components/MediaContentHeader.tsx
+++ components/MediaContentHeader.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
 import { Text, View } from './Themed';
-import { formatDate, getYear } from '@/utils/Date';
+import { getYear } from '@/utils/Date';
 
 
 const MediaContentHeader = ({
