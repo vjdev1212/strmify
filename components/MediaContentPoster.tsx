87a16eb048135a2e5b525a7f91ae85b418f42821
Jarvis <home80jarvis@gmail.com>
2025-08-05 20:11:54 +0530

    Revert changes

--- components/MediaContentPoster.tsx
+++ components/MediaContentPoster.tsx
@@ -35,7 +35,7 @@ const MediaContentPoster: React.FC<MediaContentPosterProps> = ({ background, isP
 
   const containerStyle = [
     styles.posterContainer,
-    { aspectRatio: isPortrait ? 1 / 1 : 16 / 9 }
+    { aspectRatio: isPortrait ? 4 / 5 : 16 / 9 }
   ];
 
   const posterStyle = [
