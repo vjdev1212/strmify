1eddad78bad0e347303c422022c04dacdb0366f2
Jarvis <home80jarvis@gmail.com>
2025-08-09 22:57:55 +0530

    Add react native

--- app/stream/player.tsx
+++ app/stream/player.tsx
@@ -1,27 +1,40 @@
-import MediaPlayer, { Subtitle, Chapter } from "@/components/MediaPlayer";
 import { useLocalSearchParams, useRouter } from "expo-router";
 import React from "react";
+import { Dimensions } from "react-native";
+import { VLCPlayer } from "react-native-vlc-media-player";
 
 const MediaPlayerScreen: React.FC = () => {
   const router = useRouter();
   const { videoUrl, title, artwork } = useLocalSearchParams();
-  const exampleSubtitles: Subtitle[] = [];
-
-  const exampleChapters: Chapter[] = [];
 
   const handleBack = (): void => {
     router.back();
   };
 
+  const calcVLCPlayerHeight = (windowWidth: any, aspetRatio: any) => {
+    return windowWidth * aspetRatio;
+  };
+
   return (
-    <MediaPlayer
-      videoUrl={videoUrl as string}
-      title={title as string}
-      subtitle={title as string}
-      subtitles={exampleSubtitles}
-      chapters={exampleChapters}
-      onBack={handleBack}
-      autoPlay={true}
+    <VLCPlayer
+      source={{
+        initType: 2,
+        uri:
+          'rtsp://stream.com',
+        initOptions: [
+          '--no-audio',
+          '--rtsp-tcp',
+          '--network-caching=150',
+          '--rtsp-caching=150',
+          '--no-stats',
+          '--tcp-caching=150',
+          '--realrtsp-caching=150',
+        ],
+      }}
+      autoplay={true}
+      autoAspectRatio={true}
+      resizeMode="cover"
+      style={{ height: calcVLCPlayerHeight(Dimensions.get('window').width, 3 / 4), marginTop: 30 }}
     />
   );
 };
