da40d350d546728f5bf9229f940f70e2f17f4b5a
Jarvis <home80jarvis@gmail.com>
2025-08-19 09:48:38 +0530

    Fix trakt

--- app/(tabs)/trakt.tsx
+++ app/(tabs)/trakt.tsx
@@ -1076,10 +1076,11 @@ const styles = StyleSheet.create({
         backgroundColor: 'rgba(255, 255, 255, 0.3)',
         borderBottomLeftRadius: 8,
         borderBottomRightRadius: 8,
+        overflow: 'hidden',
     },
     backdropProgressFill: {
         height: '100%',
-        backgroundColor: '#535aff',
+        backgroundColor: 'rgba(83, 90, 255, 0.75)',
         borderBottomLeftRadius: 8,
         borderBottomRightRadius: 8,
     },
