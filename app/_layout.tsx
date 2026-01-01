61f6d10299ff972e6ff801936556aafc2c0ec6fd
vjdev1212 <vjdev1212@gmail.com>
2026-01-01 12:32:37 +0530

    Dont show header inside player

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -191,7 +191,7 @@ function RootLayoutNav() {
             <Stack.Screen
               name="stream/player"
               options={{
-                headerShown: true,
+                headerShown: false,
                 headerTransparent: true,
                 headerBackground: InvisibleHeaderBackground,
                 headerTitle: '',
