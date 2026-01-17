e02c621177a5c06305c325b20d5de79390954208
vjdev1212 <vjdev1212@gmail.com>
2026-01-17 22:09:53 +0530

    Remove cast and crew shadow

--- components/MediaCastAndCrews.tsx
+++ components/MediaCastAndCrews.tsx
@@ -206,15 +206,7 @@ const styles = StyleSheet.create({
   },
   avatarWrapper: {
     position: 'relative',
-    marginBottom: 12,
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 4,
-    },
-    shadowOpacity: 0.3,
-    shadowRadius: 4.65,
-    elevation: 8,
+    marginBottom: 12   
   },
   avatar: {
     // Dynamic styles applied inline
